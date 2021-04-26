cc030b79bbd84756bbc60ab92b573da4b0932125
Ebrahim Barati Nasab <abrahim_barati@yahoo.com>
2021-04-26 17:53:14 +0430

    Save model with form and ajax without reload page

--- blog/static/blog/main.js
+++ blog/static/blog/main.js
@@ -1,34 +1,90 @@
-console.log('Hello World')
+// console.log('Hello World')
 
 const helloworld_box = document.getElementById('hello-world')
 const data_box = document.getElementById('data-box')
+const postForm = document.getElementById('post-form')
+const title = document.getElementById('id_title')
+const body = document.getElementById('id_body')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
-helloworld_box.textContent = 'Hello World x2'
+
+// helloworld_box.textContent = 'Hello World x2'
 
 
-$.ajax({
-    type : 'GET',
-    url : '/hello/',
-    success: function(response){
-        console.log('response', response)
-    },
-    error: function(error){
-        console.log('error', error)
-    },
-})
+// $.ajax({
+//     type : 'GET',
+//     url : '/hello/',
+//     success: function(response){
+//         console.log('response', response)
+//     },
+//     error: function(error){
+//         console.log('error', error)
+//     },
+// })
 
 
 $.ajax({
     type:'GET',
     url: '/data/',
     success: function(response){
-        console.log(response)
+        // console.log(response)
         const data = response.data
         data.forEach(el => {
-            data_box.innerHTML += `${el.title} - ${el.body} <br>`
+            data_box.innerHTML += `
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${el.title}</h5>
+                        <p class="card-text">${el.body}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-1">
+                                <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `
         });
     },
     error: function(error){
         console.log(error)
     },
+})
+
+
+
+postForm.addEventListener('submit', e=>{
+    e.preventDefault()
+
+    $.ajax({
+        type:'POST',
+        url:'',
+        data: {
+            'csrfmiddlewaretoken': csrf[0].value,
+            'title': title.value,
+            'body': body.value
+        },
+        success: function(response){
+            console.log(response)
+            data_box.insertAdjacentHTML('afterbegin',`
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">${response.title}</h5>
+                        <p class="card-text">${response.body}</p>
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-1">
+                                <a href="#" class="btn btn-primary">Details</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>      
+            `)
+        },
+        error: function(error){
+            console.log(error)
+        },
+    })
 })
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import News
+from .forms import NewsForm
 
 def object_list(request):
-    return render(request, 'blog/main.html')
+    form = NewsForm(request.POST or None)
+    if request.is_ajax():
+        if form.is_valid():
+            instance = form.save()
+        return JsonResponse({
+            'title': instance.title,
+            'body': instance.body,
+        })
+
+    context = {
+        'form':form,
+    }
+    return render(request, 'blog/main.html',context)
 
 def helloworld_show(request):
     return JsonResponse({'text':'Hello-world'})
